f64328d687d5459357e1a93c5e5b2e6942330107
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-05-18 11:27:23 +0300

    fixed timer

--- scripts/script.js
+++ scripts/script.js
@@ -99,15 +99,45 @@ function initMap() {
     });
 }
 
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
 async function startTimer() {
     const timerElement = document.getElementById('timer');
     if (!timerElement) return;
 
-    let timerInterval;
+    let startTime = getCookie('timerStart');
+    if (!startTime) {
+        startTime = Date.now();
+        setCookie('timerStart', startTime, 1);
+    } else {
+        startTime = parseInt(startTime, 10);
+    }
 
     function updateTimer() {
-        let startTime = Date.now();
-        timerInterval = setInterval(() => {
+        let timerInterval = setInterval(() => {
             let elapsedTime = Date.now() - startTime;
             let seconds = Math.floor((elapsedTime / 1000) % 60);
             let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
@@ -117,4 +147,8 @@ async function startTimer() {
     }
 
     updateTimer();
+
+    window.addEventListener('unload', () => {
+        eraseCookie('timerStart');
+    });
 }
